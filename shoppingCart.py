a008867882a77268191219271c9929a287761ff0
Kittipan Onlay <61462393+Eczemuth@users.noreply.github.com>
2023-04-21 23:21:08 +0700

    <feat>:Implement method

--- shoppingCart.py
+++ shoppingCart.py
@@ -1,15 +1,32 @@
+from utilities import IdGenerator
+
 class ShoppingCart:
-    def __init__(self,products):
-        self.__cart_id = ""
-        self.__products = products
+    def __init__(self):
+        self.__cart_id = IdGenerator.generate_id(id(self))
+        self.__products = []
         
     def purchase_myself(self,product,price):
         pass
+
     def purchase_myfriend(self,product,user_id,price):
         pass
-    def remove_all_product(self,product):
-        pass
-    def remove_product(self,product):
-        pass
+    
+    def remove_all_product(self):
+        self.__products = []
+
+    def remove_product(self,to_remove_product):
+        prod_dup = []
+        for product in self.__products:
+            if product not in to_remove_product:
+                prod_dup.append(product)
+
+        self.__products = prod_dup
+
     def add_to_wishlist(self,product):
         pass
+
+    def add_product(self, product):
+        self.__products.append(product)
+
+    def view_cart(self):
+        return self.__products
