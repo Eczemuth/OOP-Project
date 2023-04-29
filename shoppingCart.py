ce6a1a0ae1cdf57b27ca42f0a102ce7fd89545df
Kachen Thongsawatwong <65010100@kmitl.ac.th>
2023-04-29 23:47:26 +0700

    <feat>: update method for shoppingCart

--- shoppingCart.py
+++ shoppingCart.py
@@ -1,15 +1,30 @@
 class ShoppingCart:
-    def __init__(self,products):
-        self.__cart_id = ""
-        self.__products = products
-        
-    def purchase_myself(self,product,price):
+    def __init__(self):
+        self.__products = []
+
+    def add_product(self, product):
+        self.__products.append(product)
+
+    def get_products(self):
+        return self.__products
+
+    def view_in_cart(self):
+        name_list = []
+        for item in self.__products:
+            name_list.append(item.get_name())
+        return name_list
+
+    def purchase_myself(self, product, price):
         pass
-    def purchase_myfriend(self,product,user_id,price):
+
+    def purchase_myfriend(self, product, user_id, price):
         pass
-    def remove_all_product(self,product):
-        pass
-    def remove_product(self,product):
-        pass
-    def add_to_wishlist(self,product):
+
+    def remove_all_product(self, product):
+        self.__products = []
+
+    def remove_product(self, product):
+        self.__products.remove(product)
+
+    def add_to_wishlist(self, product):
         pass
